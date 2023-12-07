faf4c4f12ae3a141c39ebeb12daea8e5f1a181d1
salim1999 <33951489+salim1999@users.noreply.github.com>
2023-12-07 13:17:46 +0100

    neuster_Stand

---
+++ functions.py
@@ -0,0 +1,81 @@
+import requests
+
+class Functions:
+
+    @staticmethod
+    def get_images_by_agent_id(id):
+        # Create a list
+        image_list = []
+        agent_id = ""
+        api_url = f"http://127.0.0.1:8000/imagesbyagentid/{id}"
+        try:
+            response = requests.get(api_url)
+            response.raise_for_status()  # Raise an error for bad responses (4xx or 5xx)
+            json_data = response.json()
+            for data in json_data:
+                if 'base64_Image' in data:
+                    # Add the base64 image to the list
+                    image_list.append(data['base64_Image'])
+
+        except requests.exceptions.HTTPError as http_err:
+            print(f"HTTP error occurred: {http_err}")
+        except requests.exceptions.RequestException as req_err:
+            print(f"Request error occurred: {req_err}")
+
+        return image_list
+
+    @staticmethod
+    def get_images_by_base_id(id):
+        agent_id = ""
+        api_url = f"http://127.0.0.1:8000/image/{id}"
+        try:
+            response = requests.get(api_url)
+            response.raise_for_status()
+            json_data = response.json()
+
+            if 'agent_id' in json_data:
+                agent_id = json_data['agent_id']
+                return Functions.get_images_by_agent_id(agent_id)
+
+        except requests.exceptions.HTTPError as http_err:
+            print(f"HTTP error occurred: {http_err}")
+        except requests.exceptions.RequestException as req_err:
+            print(f"Request error occurred: {req_err}")
+
+        return response
+    
+
+    @staticmethod
+    def get_baseids():
+        api_url = "http://127.0.0.1:8000/baseimages/"
+        response = requests.get(api_url)
+        Baseids = []
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Parse the JSON response
+            json_data = response.json()
+            for data in json_data:
+                # Check if the 'base64_Image' node exists in the JSON data
+                if 'id' in data:
+                    id = data['id']
+                    Baseids.append(id)
+        return Baseids
+    
+    @staticmethod
+    def get_baseimage(id):
+        api_url = "http://127.0.0.1:8000/baseimage/"+str(id)
+        response = requests.get(api_url)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Parse the JSON response
+            json_data = response.json()
+            # Check if the 'base64_Image' node exists in the JSON data
+            print(json_data)
+            if 'base64' in json_data:
+                base64_Image = json_data['base64']
+                print(base64_Image)
+                return base64_Image
+    
+
--- routes.py
+++ routes.py
@@ -1,11 +1,125 @@
-from flask import Flask, render_template
+from flask import Flask, render_template ,request, jsonify
+import requests
+import base64
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, ForeignKey, DECIMAL
+
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+
+
+from functions import Functions
+import os
+from flask import Flask
+from flask_mysqldb import MySQL
+
+
 app = Flask(__name__)
+
+#Configure Database
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SECRET_KEY'] = "my super secret key that no one is supposed to know"
+
+db = SQLAlchemy(app)
+
+app.app_context().push()
+
+class User(db.Model):
+    __tablename__ = 'User'
+
+    user_id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(255), nullable=False)
+    tests = relationship('Test', back_populates='user')
+
+class ImageSet(db.Model):
+    __tablename__ = 'ImageSet'
+
+    imageset_id = Column(Integer, primary_key=True, autoincrement=True)
+    imageset_name = Column(String(255), nullable=False)
+    tests = relationship('Test', back_populates='imageset')
+
+class Test(db.Model):
+    __tablename__ = 'Test'
+
+    test_id = Column(Integer, primary_key=True, autoincrement=True)
+    time = Column(DateTime, nullable=False)
+    highscore_percentage = Column(DECIMAL(5,2), nullable=False)
+    
+    user_id = Column(Integer, ForeignKey('User.user_id'))
+    user = relationship('User', back_populates='tests')
+
+    imageset_id = Column(Integer, ForeignKey('ImageSet.imageset_id'))
+    imageset = relationship('ImageSet', back_populates='tests')
+	
+
+
 
 # two decorators, same function
 @app.route('/')
-@app.route('/index.html')
-def index():
+@app.route('/Index')
+def Index():
     return render_template('index.html', the_title='Phantomzeichner')
+
+@app.route('/get_images', methods=['GET'])
+def get_images():
+    # Assuming the request content type is set to 'application/json'
+   
+    key_value = request.args.get('number')
+    print(key_value)
+
+    return jsonify(images=Functions.get_images_by_base_id(key_value))
+
+
+
+@app.route('/baseIds')
+def BaseIds():
+    return jsonify(images=Functions.get_baseids())
+
+@app.route('/baseImage')
+def baseImage():
+    key_value = request.args.get('number')
+    print(key_value)
+    return jsonify(images=Functions.get_baseimage(key_value))
+
+
+@app.route('/Validate', methods=['GET'])
+def Validate():
+    return render_template('Validate.html', the_title='Phantomzeichner')
+
+
+@app.route('/trigger_route', methods=['POST'])
+def trigger_route():
+
+    # Assuming the request content type is set to 'application/json'
+    data = request.get_json()
+
+    # Access the value using the key
+    key_value = data.get('key')
+
+    Session = sessionmaker(bind=db.engine)
+    session = Session()
+    # Insert a new User
+    new_user = User(username='JohnDoe')
+    session.add(new_user)
+    session.commit()
+
+    # Insert a new ImageSet
+    new_imageset = ImageSet(imageset_name=key_value)
+    session.add(new_imageset)
+    session.commit()
+
+    # Insert a new Test associated with the User and ImageSet
+    new_test = Test(
+        time=datetime.now(),
+        highscore_percentage=90.5,
+        user=new_user,
+        imageset=new_imageset
+    )
+    session.add(new_test)
+    session.commit()
+
+    return "Route triggered successfully!"
 
 if __name__ == '__main__':
     app.run(debug=True)
